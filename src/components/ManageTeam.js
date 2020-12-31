041ba033406830a7821bcb2bce50294c48ff083d
Brandon Le <brandon.le@wsu.edu>
2020-12-30 21:52:37 -0800

    basic structure setup for team management page

--- src/components/ManageTeam.js
+++ src/components/ManageTeam.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 class ManageTeam extends React.Component {
   constructor() {
@@ -8,12 +8,42 @@ class ManageTeam extends React.Component {
       bench: [],
     };
   }
+
   render() {
     return (
       <div>
         <center>
-          <h3>Team</h3>
+          <div className="manage-team-container">
+            <div>
+              <h1 className="manage-team-title">Team</h1>
+              <button className="edit-team-btn">Edit Team</button>
+            </div>
+          </div>
         </center>
+        <center>
+          <h3>Starters</h3>
+        </center>
+
+        <table className="table ">
+          <thead className="thead-dark">
+            <tr>
+              <th scope="col">Name</th>
+              <th scope="col">Position</th>
+            </tr>
+          </thead>
+        </table>
+        <center>
+          <h3>Bench</h3>
+        </center>
+        <table class="table">
+          <thead class="thead-dark">
+            <tr>
+              <th scope="col">Name</th>
+              <th scope="col">Position</th>
+            </tr>
+          </thead>
+          <tbody></tbody>
+        </table>
       </div>
     );
   }
